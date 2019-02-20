e7054233bf1a014cdc2eb4a981a661c14f318e67
bmorrekyz <baltimore.kyz@gmail.com>
2019-02-19 20:32:03 -0500

    added basic authentication functionality

--- urls.py
+++ urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('login_submit', views.login_submit, name='login_submit'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('logout', views.logout, name='logout'),
 ]
--- views.py
+++ views.py
@@ -1,7 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
-from django.http import HttpResponse
 
+"""
+    authentication scenario draft 1
+    1. user opens homepage (blog/index.html)
+        if user is already logged in:
+        true: homepage has "dashboard" and "logout" links
+        false: homepage has "login" link
+
+    2. user clicks on login link
+    3. system opens login page where login form is presented to the user
+
+    4. user enters login info and clicks submit
+        if system verified these user credentials:
+        true: redirect user back to the homepage where "dashboard", "logout" links will be shown
+        false: redirect to login page
+
+"""
 
 def index(request):
-    return HttpResponse("Hello, World! Test on production")
+
+    context = {
+        "userIsLoggedIn" : request.user.is_authenticated
+    }
+
+    return render(request, 'blog/index.html', context)
+
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'blog/dashboard.html')
+    else:
+        return redirect('index')
+
+
+def login(request):
+    return render(request, 'blog/login.html')
+
+
+def login_submit(request):
+
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        auth_login(request, user)
+        return redirect('index')
+    else:
+        return redirect('login')
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
